6055f4eb67d313cdecb6234dc22fa9ecb0678fc5
afshan ahmed khan <afshanahmeda2k@gmail.com>
2024-01-27 23:59:44 +0530

    Removed dist from .gitignore and src from .npmignore

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+import { Plugin } from 'grapesjs';
+
+export type PluginOptions = {};
+declare const CarouselPlugin: Plugin<PluginOptions>;
+
+export {
+	CarouselPlugin as default,
+};
+
+export {};
